46118a87ebb4eb1dbf5bf6974d8cca3a04a8022c
anawrotsoldevelo <anawrot@soldevelo.com>
2024-02-23 13:43:35 +0100

    AO-837: Removed unused message keys

--- src/main/java/org/openlmis/ao/reports/i18n/EmbeddedReportsMessageKeys.java
+++ src/main/java/org/openlmis/ao/reports/i18n/EmbeddedReportsMessageKeys.java
@@ -5,10 +5,6 @@ public class EmbeddedReportsMessageKeys extends ReportingMessageKeys {
   private static final String ERROR = join(SERVICE_ERROR, "embeddedReport");
   public static final String ERROR_EMBEDDED_REPORT_NAME_DUPLICATED =
       join(ERROR, "name", "duplicated");
-  public static final String ERROR_EMBEDDED_REPORT_INVALID_PARAMS =
-      join(ERROR, "invalid", "request", "params");
-  public static final String ERROR_EMBEDDED_REPORT_EMPTY_PARAM =
-      join(ERROR, PARAMETER, EMPTY);
   public static final String ERROR_EMBEDDED_REPORT_NOT_FOUND = join(ERROR, NOT_FOUND);
 
 }
