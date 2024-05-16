acb9ec48d660b8db7f5449a82ce97f2e82bc3e8a
Szymon Radziszewski <117299684+sradziszewski@users.noreply.github.com>
2024-05-16 11:57:57 +0200

    ANGOLASUP-859: Removed defective migration script (#56)
    * ANGOLASUP-859: Renamed deleteEmbeddedReport method
    
    * ANGOLASUP-859: Added EmbeddedReportCategory entity with CRUD
    
    * ANGOLASUP-859: Change type for category field
    
    * ANGOLASUP-859: Fixed integration tests
    
    * ANGOLASUP-859: Removed constraint from categoryId column
    
    * ANGOLASUP-859: Removed defective migration script

--- src/main/resources/db/migration/20240326213622984__add_embedded_reports_category_constraint.sql
+++ src/main/resources/db/migration/20240326213622984__add_embedded_reports_category_constraint.sql
@@ -1,3 +1,3 @@
 ALTER TABLE reports.embedded_reports DROP COLUMN category;
-ALTER TABLE reports.embedded_reports ADD COLUMN categoryId UUID NOT NULL;
+ALTER TABLE reports.embedded_reports ADD COLUMN categoryId UUID;
 ALTER TABLE reports.embedded_reports ADD CONSTRAINT fk_embedded_reports_category FOREIGN KEY (categoryid) REFERENCES reports.embedded_report_categories(id);
