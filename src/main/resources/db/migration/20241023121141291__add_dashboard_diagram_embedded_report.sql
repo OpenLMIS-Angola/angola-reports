1801c7984fc9d92370f5a436831aaddd149ec9e1
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2024-10-23 14:39:41 +0200

    Add Diagrama do Painel embedded report

---
+++ src/main/resources/db/migration/20241023121141291__add_dashboard_diagram_embedded_report.sql
@@ -0,0 +1,7 @@
+INSERT INTO reports.embedded_reports (id, name, url, enabled, categoryid)
+VALUES (
+'3a792c67-1221-4d15-bbc9-cb3573b53e4c',
+'Diagrama do Painel',
+'https://app.powerbi.com/view?r=eyJrIjoiNGQ4YzA0NzctMjQ4Ni00MmIyLThmMjAtZTk5ZmJkY2NkN[…]6IjAwNTMxNzRhLTkxMDAtNGU4ZS05NzlhLTQ0MzZkYTAxYjBlZSIsImMiOjZ9',
+true,
+'8018303d-3c40-41ba-bf49-645fe8ba8495')
